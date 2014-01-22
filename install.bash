606bdb2176a73f2f2a9ee68377a41904813c5dd3
decebal.dobrica <decebal.dobrica@emag.ro>
2014-01-22 18:09:53 +0200

    added vim configs

--- install.bash
+++ install.bash
@@ -8,3 +8,8 @@ cat $DOTFILES/.gitconfig >> $HOME/.gitconfig
 #zsh config file
 ln -s $DOTFILES/.zshrc $HOME/.zshrc
 ln -s $DOTFILES/oh-my-zsh/ $HOME/.oh-my-zsh
+
+#vim install
+ln -s $DOTFILES/vim-sources $HOME/.vim
+ln -s $DOTFILES/.vimrc $HOME/.vimrc
+
