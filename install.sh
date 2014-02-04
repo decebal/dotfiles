dc69cc886126ddeb905517f86a5b5877066c5f51
Decebal <decebal1988@gmail.com>
2014-02-05 01:15:28 +0200

    modify links mode

--- install.sh
+++ install.sh
@@ -2,21 +2,21 @@
 DOTFILES=$HOME/.dotfiles
 
 #bash config file
-ln -s $DOTFILES/bash/bashrc $HOME/.bashrc
+ln -sbf $DOTFILES/bash/bashrc $HOME/.bashrc
 
 #zsh config file
-ln -s $DOTFILES/zsh/zshrc $HOME/.zshrc
-ln -s $DOTFILES/zsh/oh-my-zsh/ $HOME/.oh-my-zsh
+ln -sbf $DOTFILES/zsh/zshrc $HOME/.zshrc
+ln -sbf $DOTFILES/zsh/oh-my-zsh/ $HOME/.oh-my-zsh
 
 #vim install
-ln -s $DOTFILES/vim/vim-sources $HOME/.vim
-ln -s $DOTFILES/vim/vimrc $HOME/.vimrc
+ln -sbf $DOTFILES/vim/vim-sources $HOME/.vim
+ln -sbf $DOTFILES/vim/vimrc $HOME/.vimrc
 
 #git global configuration
 git config --global core.excludesfile $DOTFILES/git/gitignore_global
 cat $DOTFILES/git/gitconfig >> $HOME/.gitconfig
 
 #local configuration
-ln -h $DOTFILES/local $HOME/.local
+ln -sbf $DOTFILES/local $HOME/.local
 touch $HOME/.local/aliases
 touch $HOME/.local/functions
