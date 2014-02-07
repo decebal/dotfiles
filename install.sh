4f660dc63223a6350051b4c3d0aee1198a4bb4d3
decebal.dobrica <decebal.dobrica@emag.ro>
2014-02-07 17:24:42 +0200

    add promt and sugestive messages

--- install.sh
+++ install.sh
@@ -1,21 +1,37 @@
-#!/bin/sh
+#!/bin/bash
 DOTFILES=$HOME/.dotfiles
 
-#bash config file
-ln -sbf $DOTFILES/bash/bashrc $HOME/.bashrc
+echo "select your favorite script interpretor *********  1)bash 2)zsh 3)quit  "
+read n
+case $n in
+	1)
+		echo "creating bash links..."
+		#bash config file
+		ln -sbf $DOTFILES/bash/bashrc $HOME/.bashrc
+		;;
+	2)
+		echo "creating zsh links..."
+		#zsh config file
+		ln -sbf $DOTFILES/zsh/zshrc $HOME/.zshrc
+		ln -sbf $DOTFILES/zsh/oh-my-zsh/ $HOME/.oh-my-zsh
+		;;
+	*)
+		echo "canceled"
+		exit
+		;;
+esac
 
-#zsh config file
-ln -sbf $DOTFILES/zsh/zshrc $HOME/.zshrc
-ln -sbf $DOTFILES/zsh/oh-my-zsh/ $HOME/.oh-my-zsh
-
+echo "creating vim links..."
 #vim install
 ln -sbf $DOTFILES/vim/vim-sources $HOME/.vim
 ln -sbf $DOTFILES/vim/vimrc $HOME/.vimrc
 
+echo "setting up git config & aliases ..."
 #git global configuration
 git config --global core.excludesfile $DOTFILES/git/gitignore_global
 cat $DOTFILES/git/gitconfig >> $HOME/.gitconfig
 
+echo "creating local folder..."
 #local configuration
 ln -sbf $DOTFILES/local $HOME/.local
 
